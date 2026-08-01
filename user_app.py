f785f8ec743d6cfeba4e52b23ccc3d488164f5b9
yashwanthyash824-hub <yashwanthyash824@gmail.com>
2026-08-01 15:42:01 +0530

    Update user_app.py

--- user_app.py
+++ user_app.py
@@ -1,8 +1,8 @@
 import mystreamlit as st
 
-st.st.title("🚀 Welcome to My Custom Streamlit!")
-st.st.write("This interactive UI was generated completely from pure Python code.")
-st.st.button("Click Me!")
+st.title("🚀 Welcome to My Custom Streamlit!")
+st.write("This interactive UI was generated completely from pure Python code.")
+st.button("Click Me!")
 
 if __name__ == "__main__":
     st.run()
